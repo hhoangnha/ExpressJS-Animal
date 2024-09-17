bfb03f24ce73c33b445f26587e572374a8b74d32
Hoàng Nhã <nhanl3715@gmail.com>
2024-09-17 22:04:45 +0700

    upload db

--- config/db.js
+++ config/db.js
@@ -3,15 +3,25 @@ const mysql = require('mysql2');
 
 // Tạo pool kết nối
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'VNSnake',
-  port: 8889,
+  host: '178.16.139.195',     // Thay bằng thông tin của bạn
+  user: 'newuser',          // Thay bằng thông tin của bạn
+  password: 'newpassword',  // Thay bằng thông tin của bạn
+  database: 'VNSnake', // Thay bằng thông tin của bạn
+  port: 3306,
   waitForConnections: true,
-  connectionLimit: 100,
+  connectionLimit: 100, // Adjust as needed
   queueLimit: 0
 });
+// const pool = mysql.createPool({
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'root',
+//   database: 'VNSnake',
+//   port: 8889,
+//   waitForConnections: true,
+//   connectionLimit: 100,
+//   queueLimit: 0
+// });
 
 // Xử lý lỗi kết nối
 pool.on('error', (err) => {
